02b948db1a63e0e3bd250bb5c7ee1092c457da67
Reagan Odhiambo <errorcode.locked@gmail.com>
2024-07-23 18:37:17 +0300

    Initial Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-700 text-center py-6 font-bold">
+        Reagan Codes
+      </h1>
+    </>
+  )
+}
+
+export default App
