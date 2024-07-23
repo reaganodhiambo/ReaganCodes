43a24a7a1a69c987e0e2bb5d317a13a2936cbc28
Reagan Odhiambo <errorcode.locked@gmail.com>
2024-07-23 19:41:16 +0300

    accounts app

---
+++ Backend/accounts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_User(self, email, password=None,**extra_fields):
+        if not email:
+            raise ValueError("Email must be provided")
+        
+        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None,**extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=255, unique=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.email
+    
